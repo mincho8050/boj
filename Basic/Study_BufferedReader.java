0b26ef87296e60aa3a97276bea4a2b819b7a8aff
mincho8050 <mincho8050@gmail.com>
2019-05-31 23:20:59 +0900

    파일이동

---
+++ Basic/Study_BufferedReader.java
@@ -0,0 +1,32 @@
+package test0531;
+
+import java.io.*;
+import java.io.*;
+
+public class Study_BufferedReader {
+
+	public Study_BufferedReader(InputStreamReader inputStreamReader) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		/*
+		 * BufferedReader
+		 * > 입력방식
+		 * 
+		 * > Scanner를 통해 입력받을 경우 Space, Enter를 모두 경계로
+		 *   인식하기에 입력받은 데이터를 가공하기 매우 편리하다.
+		 * > 하지만 그에비해 BufferedReader는 Enter만 경계로 인식하고
+		 *   받은 데이터가 String으로 고정. > 데이터 가공 작업이 필요.
+		 *   Scanner에 비해 다소 사용이 불편하다 많은 양의 데이터에서는 효율이 좋음. 
+		 */
+		
+	
+		
+	Study_BufferedReader bf= new Study_BufferedReader(new InputStreamReader(System.in));
+		
+		
+
+	}//main
+
+}//class
---
+++ algorithmTest/Test10172.java
@@ -0,0 +1,18 @@
+package test0531;
+
+public class Test10172 {
+
+	public static void main(String[] args) {
+
+/* 출력값
+		|\_/|
+		|q p|   /}
+		( 0 )"""\
+		|"^"`    |
+		||_/=\\__|
+*/	
+		
+
+	}
+
+}
