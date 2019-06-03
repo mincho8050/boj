d6252f42c1bc2994f2e92abc3c25cfc47d1ba173
mincho8050 <mincho8050@gmail.com>
2019-06-04 01:15:26 +0900

    study_Scanner 내용 수정

--- Basic/Study_Scanner.java
+++ Basic/Study_Scanner.java
@@ -1,4 +1,4 @@
-package Basic;
+package test0531;
 
 import java.util.Scanner;
 //import java.lang.*;
@@ -23,6 +23,66 @@ public class Study_Scanner {
 		            //Scanner가 쓸데없이 계속 입력을 받는 것을 방지하기 위함.
 		
 		System.out.println(name+":"+Math);
+		
+
+		/*
+		 * Scanner  
+		 * > 키보드로 인해 입력된 문자열을 받아서 처리해주는 클래스
+		 * 
+		 * Scanner 선언
+		 * > import java.util.*; 
+		 * > 이건 util 안에 있는 모든 클래스를 선언.
+		 * 
+		 * Scanner 생성
+		 * > Scanner input=new Scanner(System.in); 
+		 * >키보드로 문자열을 입력받기 위해 인자를 System.in으로 설정
+		 * 
+		 * Scanner 확인
+		 * > while(input.hasNextLine())
+		 * > 행이 있다면 true, 없다면 false 리턴
+		 * 
+		 * Scanner로 받은 데이터 저장하지
+		 * > in=input.nextLine(); 
+		 * > input.nextLine(); 문자열 반환 함수,엔터만나면 종료
+		 * 
+		 * Scanner 종료
+		 * > input.close();
+		 * > 종료를 해주지 않으면 메모리 낭비
+		 * 
+		 */
+		
+		
+		/*
+		 * hasNextInt() 
+		 * > 입력값이 int형이 아닐 경우 false 리턴
+		 * 
+		 * hasNextLine()
+		 * > 행이 있다면 true, 없다면 false 리턴
+		 * 
+		 * hasNext()
+		 * > 토큰이 있다면 true, 없다면 false 리턴
+		 */
+		
+		/*
+		 * Scanner 반환 함수
+		 * 
+		 * next()
+		 * > 문자열 반환함수. 공백(space)를 만나면 종료
+		 * 
+		 * nextLine()
+		 * > 문자열 반환함수. 개행(Enter)를 만나면 종료된다.
+		 * > next()와 달리 띄어쓰기를 할 수 있다. 
+		 * 
+		 * nextInt()
+		 * > int 타입 반환 함수
+		 * 
+		 * nextDouble()
+		 * > double 타입 반환 함수
+		 * 
+		 * nextFloat()
+		 * > float 타입 반환 함수
+		 */
+		
 		
 		
 		
